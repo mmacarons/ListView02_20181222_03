91f577a2ecded0ff7362e7c7b62476db5e8e9f81
manymany.macarons <manymany.macarons@gmail.com>
2018-12-22 16:20:59 +0900

    자동 바인딩 테스트

--- app/src/main/java/kr/tjit/listview02_20181222_03/StoreDetailActivity.java
+++ app/src/main/java/kr/tjit/listview02_20181222_03/StoreDetailActivity.java
@@ -1,7 +1,7 @@
 package kr.tjit.listview02_20181222_03;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import kr.tjit.listview02_20181222_03.datas.Store;
@@ -9,11 +9,13 @@ import kr.tjit.listview02_20181222_03.datas.Store;
 public class StoreDetailActivity extends BaseActivity {
 
 //    클래스의 멤버변수로 받아올 데이터 변수를 생성.
-    Store mStore;
+Store mStore;
+    private android.widget.ImageView logoImg;
+    private TextView temㅈpTxt;
+    private TextView nameTxt;
+    private TextView addressTxt;
+    private TextView openTimeTxt;
 
-    TextView nameTxt;
-    TextView addressTxt;
-    TextView openTimeTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +48,11 @@ public class StoreDetailActivity extends BaseActivity {
     @Override
     public void bindViews() {
 
-        nameTxt = findViewById(R.id.nameTxt);
-        addressTxt = findViewById(R.id.addressTxt);
-        openTimeTxt = findViewById(R.id.openTimeTxt);
+        this.openTimeTxt = (TextView) findViewById(R.id.openTimeTxt);
+        this.addressTxt = (TextView) findViewById(R.id.addressTxt);
+        this.nameTxt = (TextView) findViewById(R.id.nameTxt);
+        this.tempTxt = (TextView) findViewById(R.id.tempTxt);
+        this.logoImg = (ImageView) findViewById(R.id.logoImg);
 
     }
 }
