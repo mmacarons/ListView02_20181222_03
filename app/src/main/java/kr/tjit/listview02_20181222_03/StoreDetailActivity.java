bdc07bf6d4c1ab27ee114763e38648a67ad874b2
manymany.macarons <manymany.macarons@gmail.com>
2018-12-22 15:44:36 +0900

    가게 상세화면 xml 작성

---
+++ app/src/main/java/kr/tjit/listview02_20181222_03/StoreDetailActivity.java
@@ -0,0 +1,33 @@
+package kr.tjit.listview02_20181222_03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoreDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_detail);
+
+        bindViews();
+        setupEvents();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
